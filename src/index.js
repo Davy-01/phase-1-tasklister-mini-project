7de2f25558c443e470460f4c339edcb67c303301
Davis <daviesnyakiba@gmail.com>
2023-09-30 23:59:06 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e)=>{
+      e.preventDefault();
+    handleToDo(e.target['new-task-description'].value)
+    
+  })
+  });
+
+function handleToDo(description){
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "x"
+  let list = document.createElement("li");
+  list.textContent = description;
+  list.appendChild(btn)
+   document.querySelector("#tasks").appendChild(list)
+  
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
